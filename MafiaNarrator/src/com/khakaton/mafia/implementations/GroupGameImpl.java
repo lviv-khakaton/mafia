5ba68ca01f6107a9263f6935728f46f24fa18d95
Ivan <mylyanyk.ivan@gmail.com>
2012-08-25 17:23:09 +0300

    empty impl class added

---
+++ MafiaNarrator/src/com/khakaton/mafia/implementations/GroupGameImpl.java
@@ -0,0 +1,25 @@
+package com.khakaton.mafia.implementations;
+
+import com.khakaton.mafia.interfaces.GroupGame;
+
+public class GroupGameImpl implements GroupGame {
+
+	@Override
+	public void start() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void play() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void doCycle() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
