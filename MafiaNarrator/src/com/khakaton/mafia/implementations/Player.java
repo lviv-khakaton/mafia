3352929e07f481b140d254485d8761a4ab97b5c0
Ivan <mylyanyk.ivan@gmail.com>
2012-08-25 19:19:43 +0300

    player class changed a little.

--- MafiaNarrator/src/com/khakaton/mafia/implementations/Player.java
+++ MafiaNarrator/src/com/khakaton/mafia/implementations/Player.java
@@ -3,6 +3,8 @@ package com.khakaton.mafia.implementations;
 public class Player {
 	private PlayerType type;
 	private Boolean alive;
+	private int decision;
+	private String playerName; 
 	
 	public Player(PlayerType type){
 		this.alive=true;
@@ -20,5 +22,21 @@ public class Player {
 	}
 	public void setAlive(Boolean alive) {
 		this.alive = alive;
+	}
+
+	public int getDecision() {
+		return decision;
+	}
+
+	public void setDecision(int decision) {
+		this.decision = decision;
+	}
+
+	public String getPlayerName() {
+		return playerName;
+	}
+
+	public void setPlayerName(String playerName) {
+		this.playerName = playerName;
 	}
 }
