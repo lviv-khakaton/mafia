05f5443cb4f85bf8430db1c396d58e770bab4209
Ivan <mylyanyk.ivan@gmail.com>
2012-08-25 20:27:32 +0300

    basic client and server added.

--- MafiaNarrator/src/com/khakaton/mafia/implementations/MafiaClient.java
+++ MafiaNarrator/src/com/khakaton/mafia/implementations/MafiaClient.java
@@ -1,13 +1,25 @@
 package com.khakaton.mafia.implementations;
 
+import java.io.*;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
 public class MafiaClient {
 
 	/**
 	 * @param args
+	 * @throws IOException 
+	 * @throws UnknownHostException 
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws UnknownHostException, IOException {
 		// TODO Auto-generated method stub
-
+		Socket socket = new Socket("localhost", 4444);
+		Scanner s = new Scanner(System.in);
+		String name = s.nextLine();
+		DataOutputStream os = new DataOutputStream(socket.getOutputStream());
+		DataInputStream is = new DataInputStream(socket.getInputStream());
+		os.writeUTF(name);
 	}
 
 }
