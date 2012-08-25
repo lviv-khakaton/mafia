d368abf7bebdc91dd794a565c7330e8b18802296
Ivan <mylyanyk.ivan@gmail.com>
2012-08-25 18:14:28 +0300

    player edited
    getters and setters.

--- MafiaNarrator/src/com/khakaton/mafia/implementations/Player.java
+++ MafiaNarrator/src/com/khakaton/mafia/implementations/Player.java
@@ -1,6 +1,19 @@
 package com.khakaton.mafia.implementations;
 
 public class Player {
-	PlayerType type;
-	Boolean alive;
+	private PlayerType type;
+	private Boolean alive;
+	
+	public PlayerType getType() {
+		return type;
+	}
+	public void setType(PlayerType type) {
+		this.type = type;
+	}
+	public Boolean getAlive() {
+		return alive;
+	}
+	public void setAlive(Boolean alive) {
+		this.alive = alive;
+	}
 }
