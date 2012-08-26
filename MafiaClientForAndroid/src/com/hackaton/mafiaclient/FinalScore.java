1b9e62a7c3c8d3d47529d0383a625d78b387134e
Ivan <mylyanyk.ivan@gmail.com>
2012-08-26 11:52:51 +0300

    ogo

--- MafiaClientForAndroid/src/com/hackaton/mafiaclient/FinalScore.java
+++ MafiaClientForAndroid/src/com/hackaton/mafiaclient/FinalScore.java
@@ -2,17 +2,20 @@ package com.hackaton.mafiaclient;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class FinalScore extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        super.setContentView(R.layout.activity_final);
+        
+        Log.i("#FS","step 1");
         TextView tv = (TextView)findViewById(R.id.result);
+        Log.i("#FS","step 2");
         if(Magic.pm.won)
         	tv.setText("Congratulation, you team won this round.");
         else
         	tv.setText("What a pity, you've loosed this round. Don't worry, try another time.");
-        	
-        super.setContentView(R.layout.activity_final);
     }
 }
